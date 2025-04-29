3f6bf3281c7751e87f03ecb14ede49e2589817ea
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-04-29 21:38:14 +0530

    make model files

---
+++ model/CustomerModel.js
@@ -0,0 +1,9 @@
+export default class CustomerModel {
+
+    constructor(fname, lname, contact, address) {
+        this.fname = fname;
+        this.lname = lname;
+        this.contact = contact;
+        this.address = address;
+    }
+}
---
+++ model/ItemModel.js
@@ -0,0 +1,8 @@
+export default class ItemModel {
+
+    constructor(description, price, qty) {
+        this.description = description;
+        this.price = price;
+        this.qty = qty;
+    }
+}
