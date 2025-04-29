96e61b44d7a4c59bdd73148c853aa3af142c6ddf
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-04-29 21:07:57 +0530

    make project as single page application

--- style_js/controller.js
+++ style_js/controller.js
@@ -1,4 +1,27 @@
-$('#home-btn').on('click', function() {
+$('.home-btn').on('click', function() {
+    $('#home-sec').css('display', 'block');
+    $('#customer-sec').css('display', 'none');
+    $('#item-sec').css('display', 'none');
+    $('#order-sec').css('display', 'none');
+});
+
+$('.customer-btn').on('click', function() {
+    $('#home-sec').css('display', 'none');
+    $('#customer-sec').css('display', 'block');
+    $('#item-sec').css('display', 'none');
+    $('#order-sec').css('display', 'none');
+});
+
+$('.item-btn').on('click', function() {
+    $('#home-sec').css('display', 'none');
     $('#customer-sec').css('display', 'none');
     $('#item-sec').css('display', 'block');
+    $('#order-sec').css('display', 'none');
+});
+
+$('.order-btn').on('click', function() {
+    $('#home-sec').css('display', 'none');
+    $('#customer-sec').css('display', 'none');
+    $('#item-sec').css('display', 'none');
+    $('#order-sec').css('display', 'block');
 });
