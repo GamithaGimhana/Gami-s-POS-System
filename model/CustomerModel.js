c44b3dd12d71755f5e6cd68e422e59697d8fcf25
GamithaGimhana <gamitha.gimhana99@gmail.com>
2025-05-03 12:17:50 +0530

    add ids to models

--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -1,6 +1,7 @@
 export default class CustomerModel {
 
-    constructor(fname, lname, contact, address) {
+    constructor(customer_id, fname, lname, contact, address) {
+        this.customer_id = customer_id;
         this.fname = fname;
         this.lname = lname;
         this.contact = contact;
